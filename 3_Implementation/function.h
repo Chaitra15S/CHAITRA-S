9ab49a150a83c3efc25c5c138f7e82bbd7cb74d6
Chaitra15S <86303252+Chaitra15S@users.noreply.github.com>
2021-07-12 22:44:23 +0530

    Create function.h

---
+++ 3_Implementation/function.h
@@ -0,0 +1,20 @@
+/**
+ * @file function.h
+ * @author chaitra s
+ * @brief the function header files
+ * @version 0.1
+ * @date 2021-07-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __FUNCTION_H__
+#define ___FUNCTION_H__
+
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+void edit_score(float score, char plnm[20]);
+
+#endif
