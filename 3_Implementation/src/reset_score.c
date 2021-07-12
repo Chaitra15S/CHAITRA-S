1b3a00f0aec4ebe95f964da54941919cca03af82
Chaitra15S <86303252+Chaitra15S@users.noreply.github.com>
2021-07-12 22:37:31 +0530

    Create reset_score.c

---
+++ 3_Implementation/src/reset_score.c
@@ -0,0 +1,25 @@
+/**
+ * @file reset_score.c
+ * @author chaitra s
+ * @brief this function is used to reset scores
+ * @version 0.1
+ * @date 2021-07-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "../function.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void reset_score()
+    {system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%19s%f",nm,&sc);
+	sc=0;
+	fprintf(f,"19%s,%.2f",nm,sc);
+    fclose(f);}
